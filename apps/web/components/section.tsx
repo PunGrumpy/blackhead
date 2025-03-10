7ba5623ac816d15770884294d3449a755b9e279a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-10 23:28:49 +0700

    feat(section): add Cross component for decorative purposes and adjust layout

--- apps/web/components/section.tsx
+++ apps/web/components/section.tsx
@@ -1,6 +1,15 @@
 import { cn } from '@repo/ui/lib/utils'
 import type { HTMLAttributes, ReactNode } from 'react'
 
+const Cross = () => {
+  return (
+    <div className="relative grid h-6 w-6 self-center">
+      <div className="absolute h-6 w-3 border-muted-foreground border-r" />
+      <div className="absolute h-3 w-6 border-muted-foreground border-b" />
+    </div>
+  )
+}
+
 interface SectionProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode
   className?: string
@@ -9,14 +18,11 @@ interface SectionProps extends HTMLAttributes<HTMLDivElement> {
 export const Section = ({ children, className, ...props }: SectionProps) => {
   return (
     <section {...props}>
-      <div className="container relative mx-auto">
+      <div className="relative mx-auto max-w-[1080px]">
         <div className={cn('sm:border-x', className)}>{children}</div>
-        {/* <div className="-bottom-3 -left-3 absolute z-10 hidden h-6 sm:block">
+        <div className="-top-3 -left-3 absolute z-10 hidden h-6 sm:block">
           <Cross />
         </div>
-        <div className="-bottom-3 -right-3 -translate-x-px absolute z-10 hidden h-6 sm:block">
-          <Cross />
-        </div> */}
       </div>
     </section>
   )
