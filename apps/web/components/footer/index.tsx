abf12ba1db8f3b7df62ed7053cdef8a6dfd5fc81
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-13 03:56:19 +0700

    refactor(footer): update grid layout for improved responsiveness and alignment

--- apps/web/components/footer/index.tsx
+++ apps/web/components/footer/index.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
     <footer className="mx-4 bg-background py-8 sm:py-24">
       <nav aria-label="Blackhead Directory">
         <div className="mx-auto flex max-w-[1080px] flex-col gap-4 sm:gap-16 ">
-          <div className="grid w-full grid-cols-[repeat(4,_1fr)_80px]">
+          <div className="grid w-full grid-cols-2 gap-8 md:grid-cols-[repeat(4,_1fr)_80px] md:gap-0">
             {FooterNavigationItems.map(section => (
               <div key={section.title} className="space-y-4">
                 <h2 className="text-sm tracking-wider">{section.title}</h2>
@@ -29,7 +29,7 @@ export const Footer = () => {
               </div>
             ))}
 
-            <div className="flex justify-end">
+            <div className="col-span-full row-span-full flex justify-start md:col-span-1 md:row-span-1 md:justify-end">
               <Link href="/home" className="block">
                 <div className="relative h-7 w-7 overflow-hidden">
                   <div className="absolute inset-0 flex items-center justify-center">
