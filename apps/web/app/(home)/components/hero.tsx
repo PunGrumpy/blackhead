82f93f66dce5da752e9628241f0472787a070a10
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-13 20:47:21 +0700

    refactor(hero): update Hero component with dashed background for improved visual appeal

--- apps/web/app/(home)/components/hero.tsx
+++ apps/web/app/(home)/components/hero.tsx
@@ -3,7 +3,7 @@ import { Button } from '@repo/ui/components/ui/button'
 
 export const Hero = () => {
   return (
-    <Section className="border-y p-10">
+    <Section className="border-y bg-dashed p-10">
       <div className="flex flex-col gap-8 sm:items-center">
         <h1 className="text-center font-bold text-2xl sm:text-4xl lg:text-5xl">
           Blackhead
