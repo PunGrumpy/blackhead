4ce30ebe425036a5d7e45d709a00de00e2cc93d7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-22 08:44:06 +0700

    feat(auth): add inferAdditionalFields plugin for type inference

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,9 +1,13 @@
-import { passkeyClient } from 'better-auth/client/plugins'
+import {
+  inferAdditionalFields,
+  passkeyClient
+} from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 import { keys } from './keys'
+import type { auth } from './server'
 
 export const { signIn, signOut, signUp, useSession } = createAuthClient({
   baseURL: keys().NEXT_PUBLIC_API_URL,
   basePath: '/webhooks/better-auth',
-  plugins: [passkeyClient()]
+  plugins: [inferAdditionalFields<typeof auth>(), passkeyClient()]
 })
