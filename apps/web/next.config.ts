43b8b65f955e0f9d639dd1940e82af7f81641288
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-23 22:30:06 +0700

    feat(config): add CORS headers configuration in next.config for improved API integration

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,6 @@
 import { config } from '@repo/next-config'
 import type { NextConfig } from 'next'
+import { env } from './env'
 
 const nextConfig: NextConfig = {
   ...config,
@@ -21,6 +22,36 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     nodeMiddleware: true
+  },
+  // biome-ignore lint/suspicious/useAwait: headers want to use async functions
+  headers: async () => {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: env.NEXT_PUBLIC_API_URL
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization'
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
+          },
+          {
+            key: 'Access-Control-Max-Age',
+            value: '600'
+          }
+        ]
+      }
+    ]
   }
 }
 
