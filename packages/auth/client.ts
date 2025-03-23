82670d26185d31764d3780a602286c4612ced00c
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-23 13:45:15 +0700

    refactor(auth): enhance plugin configuration to include user role type

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -6,5 +6,13 @@ import type { auth } from './server'
 export const { signIn, signOut, signUp, useSession } = createAuthClient({
   baseURL: keys().NEXT_PUBLIC_API_URL,
   basePath: '/webhooks/better-auth',
-  plugins: [inferAdditionalFields<typeof auth>()]
+  plugins: [
+    inferAdditionalFields<typeof auth>({
+      user: {
+        role: {
+          type: 'string'
+        }
+      }
+    })
+  ]
 })
