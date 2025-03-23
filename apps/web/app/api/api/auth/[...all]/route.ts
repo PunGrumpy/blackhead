68ff836dcec1f2ca6da2e8a79f2152e0a94e8c9b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-24 00:24:13 +0700

    fix(auth): comment out edge runtime due to Hobby plan limitations

--- apps/web/app/api/api/auth/[...all]/route.ts
+++ apps/web/app/api/api/auth/[...all]/route.ts
@@ -1,5 +1,5 @@
 import { auth, toNextJsHandler } from '@repo/auth/server'
 
-export const runtime = 'edge'
+// export const runtime = 'edge' // Hobby plan not is limited to edge runtime 1mb
 
 export const { POST, GET } = toNextJsHandler(auth)
