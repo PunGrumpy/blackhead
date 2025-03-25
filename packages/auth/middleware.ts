950b62cd5df1753caca54659ef294e0dcddd0b5a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-25 16:58:44 +0700

    refactor(auth): simplify session retrieval in auth middleware by using cookie utility

--- packages/auth/middleware.ts
+++ packages/auth/middleware.ts
@@ -1,21 +1,12 @@
+import { getSessionCookie } from 'better-auth/cookies'
 import { type NextRequest, NextResponse } from 'next/server'
-import { keys } from './keys'
 
 const isProtectedRoute = (request: NextRequest) => {
   return request.url.includes('/dashboard')
 }
 
-export const authMiddleware = async (request: NextRequest) => {
-  const url = new URL(
-    `${keys().NEXT_PUBLIC_API_URL}/webhooks/better-auth/get-session`
-  )
-  const response = await fetch(url, {
-    headers: {
-      cookie: request.headers.get('cookie') || ''
-    }
-  })
-
-  const session = await response.json()
+export const authMiddleware = (request: NextRequest) => {
+  const session = getSessionCookie(request)
 
   if (isProtectedRoute(request) && !session) {
     return NextResponse.redirect(new URL('/sign-in', request.url))
