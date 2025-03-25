9e0ffd79ffe52557ebc271c864c1c190c6b9fe82
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-03-25 15:50:48 +0700

    refactor(auth): change to use api

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,7 +1,10 @@
 import { inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
+import { keys } from './keys'
 import type { auth } from './server'
 
 export const { signIn, signOut, signUp, useSession } = createAuthClient({
+  baseURL: keys().NEXT_PUBLIC_API_URL,
+  basePath: '/webhooks/better-auth',
   plugins: [inferAdditionalFields<typeof auth>()]
 })
