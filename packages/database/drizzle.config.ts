140726bdbdaadef937e74c27ba4348e22b361b3f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-04-04 01:42:14 +0700

    refactor(database): restructure database package by updating schema paths, removing unused files, and adding a new index file for database connection

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit'
 import { keys } from './keys'
 
 export default {
-  schema: './src/schema.ts',
+  schema: './schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: keys().DATABASE_URL
--- packages/database/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { drizzle } from 'drizzle-orm/neon-http'
-import { keys } from '../keys'
-
-export const db = drizzle(keys().DATABASE_URL, {})
