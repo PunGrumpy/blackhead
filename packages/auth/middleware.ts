0d8f9675bc2ad8bd112501ffdff4b3dac5eb0207
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-04-04 18:16:39 +0700

    refactor(auth): simplify authentication middleware by replacing better-fetch with session cookie retrieval

--- packages/auth/middleware.ts
+++ packages/auth/middleware.ts
@@ -1,26 +1,15 @@
-import { betterFetch } from '@better-fetch/fetch'
+import { getSessionCookie } from 'better-auth/cookies'
 import { type NextRequest, NextResponse } from 'next/server'
-import type { auth } from './server'
-
-type Session = typeof auth.$Infer.Session
 
 const isProtectedRoute = (request: NextRequest) => {
   return request.url.includes('/dashboard')
 }
 
-export const authMiddleware = async (request: NextRequest) => {
-  const { data: session } = await betterFetch<Session>(
-    '/api/auth/get-session',
-    {
-      baseURL: request.nextUrl.origin,
-      headers: {
-        cookie: request.headers.get('cookie') || '' // Forward the cookies from the request
-      }
-    }
-  )
+export const authMiddleware = (request: NextRequest) => {
+  const session = getSessionCookie(request)
 
   if (isProtectedRoute(request) && !session) {
-    return NextResponse.redirect(new URL('/sign-in', request.url))
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
   return NextResponse.next()
