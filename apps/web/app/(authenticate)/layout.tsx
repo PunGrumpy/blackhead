03e37054fa16a56f9850030ab6153a79b7dc0842
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-04-06 20:54:02 +0700

    refactor(auth): implement session check in AuthLayout to redirect logged-in users to dashboard

--- apps/web/app/(authenticate)/layout.tsx
+++ apps/web/app/(authenticate)/layout.tsx
@@ -1,3 +1,6 @@
+import { auth } from '@repo/auth/server'
+import { headers } from 'next/headers'
+import { RedirectType, redirect } from 'next/navigation'
 import type { ReactNode } from 'react'
 import { Footer } from './components/footer'
 import { Header } from './components/header'
@@ -6,7 +9,16 @@ interface AuthLayoutProps {
   children: ReactNode
 }
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+  const headersList = await headers()
+  const session = await auth.api.getSession({
+    headers: headersList
+  })
+
+  if (session) {
+    redirect('/dashboard', RedirectType.replace) // Redirect to dashboard if already logged in
+  }
+
   return (
     <>
       <Header />
