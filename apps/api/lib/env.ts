9631a88c4968739f327ef0eb7d810d0721e072c4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-04-08 19:55:53 +0700

    refactor(env): reorder environment configuration extensions for improved clarity

--- apps/api/lib/env.ts
+++ apps/api/lib/env.ts
@@ -5,7 +5,7 @@ import { keys as core } from '@repo/next-config/keys'
 import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
-  extends: [core(), database(), analytics()],
+  extends: [core(), analytics(), database()],
   server: {},
   client: {},
   runtimeEnv: {}
