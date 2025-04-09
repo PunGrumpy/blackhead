77f55c0eaf2e37c50b5bfeda7390067b3fdca577
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-04-10 01:05:58 +0700

    feat(layout): enhance authenticated layout with header, footer, and navigation components
    - Added Header, Footer, and Navigation components to the authenticated layout for improved user experience.
    - Integrated avatar display in the Header using the new avatar service.
    - Updated layout structure to include the new components, ensuring a cohesive design.
    - Introduced no-scrollbar utility in global styles for better aesthetics.

--- apps/web/app/(authenticated)/layout.tsx
+++ apps/web/app/(authenticated)/layout.tsx
@@ -2,6 +2,9 @@ import { auth } from '@repo/auth/server'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 import type { ReactNode } from 'react'
+import { Footer } from './components/footer'
+import { Header } from './components/header'
+import { Naviation } from './components/navigation'
 
 interface DashboardLayoutProps {
   children: ReactNode
@@ -19,5 +22,14 @@ export default async function DashboardLayout({
     redirect('/login') // Redirect to login if not logged in
   }
 
-  return <>{children}</>
+  return (
+    <>
+      <Header />
+      <main className="min-h-[calc(100vh-64px)]">
+        <Naviation />
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
 }
